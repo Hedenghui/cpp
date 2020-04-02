75df7c8fe794981ee4c72f38071e9ee70b61a0e0
贺登辉 <1143671646@qq.com>
2020-04-02 23:51:11 +0800

    c++入门练习

---
+++ 2020.4.3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int a = 10;
+int b = 20;
+int main()
+{
+	cout << "hello world" << endl;
+	cout << a << " " << b << endl;
+	system("pause");
+	return 0;
+}
