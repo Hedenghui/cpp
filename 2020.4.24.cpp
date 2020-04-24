651ec676e3c6b3554bfb6163b9276301abe8e367
贺登辉 <1143671646@qq.com>
2020-04-24 23:56:05 +0800

    类与对象练习代码

---
+++ 2020.4.24.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	Date(int year = 0, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void print()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	Date d1;
+	Date d2(d1);
+	d1.print();
+	d2.print();
+	return 0;
+}
