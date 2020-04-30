c544ec5463b423ebc5c13bc9396992dbd5bf70c2
贺登辉 <1143671646@qq.com>
2020-04-30 23:16:27 +0800

    浅拷贝的学习

---
+++ 2020.4.29.cpp
@@ -0,0 +1,143 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+using namespace std;
+
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//	void operator=(const Date& d)
+//	{
+//		if (this != &d)
+//		{
+//			_year = d._year;
+//			_month = d._month;
+//			_day = d._day;
+//		}
+//	}
+//	void print()
+//	{
+//		cout <<_year<<_month<<_day<< endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	Date d2(2008, 1, 1);
+//	d1 = d2;
+//	d1.print();
+//	d2.print();
+//	return 0;
+//}
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//		cout << "Date()" << endl;
+//	}
+//	Date& operator=(const Date& d)
+//	{
+//		if (this != &d)
+//		{
+//			_year = d._year;
+//			_month = d._month;
+//			_day = d._day;
+//		}
+//		return *this;
+//	}
+//	void print()
+//	{
+//		cout <<_year<<_month<<_day<< endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	Date d2(2008, 1, 1);
+//	d1 = d2;
+//	d1.print();
+//	d2.print();
+//
+//	return 0;
+//}
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	void print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	Date d2(2018,10, 1);
+//	// 这里d1调用的编译器生成operator=完成拷贝，d2和d1的值也是一样的。
+//	d1 = d2;
+//	d1.print();
+//	d2.print();
+//	return 0;
+//}
+ //这里会发现下面的程序会崩溃掉？这里就需要我们以后讲的深拷贝去解决。
+class String
+{
+public:
+	String(const char* str = "")
+	{
+		_str = (char*)malloc(strlen(str) + 1);
+		strcpy(_str, str);
+	}
+	~String()
+	{
+		cout << "~String()" << endl;
+		free(_str);
+	}
+private:
+	char* _str;
+};
+int main()
+{
+	String s1("hello");
+	String s2(s1);
+	return 0;
+}
