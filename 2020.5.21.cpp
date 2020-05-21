e79f712d28eddd226db5593a2434756d416abf6e
贺登辉 <1143671646@qq.com>
2020-05-21 22:40:44 +0800

    oj 只出现一次的数字2 代码

---
+++ 2020.5.21.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		//统计出所有数32个位1出现的次数
+		int bitsArray[32] = { 0 };
+		for (auto val : nums)
+		{
+			for (size_t i = 0; i<32; i++)
+			{
+				if (val&(1 << i))
+				{
+					bitsArray[i]++;
+				}
+			}
+		}
+		int num = 0;
+		for (size_t i = 0; i<32; i++)
+		{
+			if (bitsArray[i] % 3 == 1)
+			{
+				num |= (1 << i);
+			}
+		}
+		return num;
+	}
+};
+
+int main()
+{
+	return 0;
+}
