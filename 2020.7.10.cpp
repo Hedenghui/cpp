a050dba1841468008f2163c8ca417e184387266d
Hedenghui <1143671646@qq.com>
2020-07-10 21:55:43 +0800

    oj 客似云来 代码

---
+++ 2020.7.10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int from, to;
+	while (cin >> from >> to)
+	{
+		long sum = 0, a = 1, b = 1;
+		for (int i = 0; i < to; ++i)
+		{
+			if (i >= from - 1)
+				sum += a;
+			swap(a, b);
+			b += a;
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
