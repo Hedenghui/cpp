c6a013ca09548883e632e898167a5e880dfc3117
Hedenghui <1143671646@qq.com>
2020-10-13 23:14:58 +0800

    oj字符串中的第一个唯一字符

---
+++ 2020.10.13.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+#include<string>
+class Solution {
+public:
+	int firstUniqChar(string s) {
+		int* a = new int[128]{0};
+		for (int i = 0; i<s.size(); i++)
+		{
+			a[s[i]]++;
+		}
+		for (int i = 0; i<s.size(); i++)
+		{
+			if (a[s[i]] == 1)
+				return i;
+		}
+		return -1;
+	}
+};
+int main()
+{
+	return 0;
+}
