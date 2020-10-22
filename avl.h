abb4b170efc2643d42a2bbfb9bfb187e0e4ff9b7
Hedenghui <1143671646@qq.com>
2020-10-22 21:51:12 +0800

    自己实现的AVL树

---
+++ avl.h
@@ -0,0 +1,272 @@
+#pragma once
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+
+	int _bf;//平衡因子
+
+	pair<K, V> _kv;
+
+	AVLTreeNode(const pair<K, V>& kv)
+		: _left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _kv(kv)
+		, _bf(0)
+	{}
+};
+
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		//1.先按搜索树的规则进行插入
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+			cur->_parent = parent;
+		}
+		else
+		{
+			parent->_left = cur;
+			cur->_parent = parent;
+		}
+
+		while (parent)
+		{
+			if (cur == parent->_left)
+				parent->_bf--;
+			else
+				parent->_bf++;
+
+			if (parent->_bf == 0)
+				break;
+			else if (parent->_bf == 1 || parent->_bf == -1)
+			{
+				cur = parent;
+				parent = parent->_parent;
+			}
+			else if (parent->_bf == 2 || parent->_bf == -2)
+			{
+				if (parent->_bf == 2)
+				{
+					if (cur->_bf == 1)
+						RotateL(parent);
+					else if (cur->_bf == -1)
+						RotateRL(parent);
+				}
+				else if (parent->_bf == -2)
+				{
+					if (cur->_bf == 1)
+						RotateLR(parent);
+					else if (cur->_bf == -1)
+						RotateR(parent);
+				}
+				break;
+			}
+			
+		}
+		return true;
+
+	}
+
+
+	//左旋
+	void RotateL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		parent->_right = subRL;
+		if (subRL)
+		{
+			subRL->_parent = parent;
+		}
+		subR->_left = parent;
+		Node* ppNode = parent->_parent;
+		parent->_parent = subR;
+
+		if (_root == parent)
+		{
+			_root = subR;
+			subR->_parent = nullptr;
+		}
+		else
+		{
+			subR->_parent = ppNode;
+			if (ppNode->_left==parent)
+				ppNode->_left = subR;
+			else
+				ppNode->_right = subR;
+		}
+		parent->_bf = subR->_bf = 0;
+	}
+
+	void RotateR(Node* parent)
+	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+		parent->_left = subLR;
+		if (subLR)
+		{
+			subLR->_parent = parent;
+		}
+		subL->_right = parent;
+		Node* ppNode = parent->_parent;
+		parent->_parent = subL;
+
+		if (_root == parent)
+		{
+			_root = subL;
+			subL->_parent = nullptr;
+		}
+		else
+		{
+			subL->_parent = ppNode;
+			if (ppNode->_left == parent)
+				ppNode->_left = subL;
+			else
+				ppNode->_right = subL;
+		}
+		parent->_bf = subL->_bf = 0;
+	}
+
+	void RotateRL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		int bf = subRL->_bf;
+
+		RotateR(subR);
+		RotateL(parent);
+		if (bf == -1)
+		{
+			subR->_bf = 1;
+			parent->_bf = 0;
+			subRL->_bf = 0;
+		}
+		else if (bf == 1)
+		{
+			parent->_bf = -1;
+			subR->_bf = 0;
+			subRL->_bf = 0;
+		}
+		
+	}
+	void RotateLR(Node* parent)
+	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+		int bf = subLR->_bf;
+		RotateL(subL);
+		RotateR(parent);
+		if (bf == -1)
+		{
+			subL->_bf = 0;
+			parent->_bf = 1;
+			subLR->_bf = 0;
+		}
+		else if (bf == 1)
+		{
+			parent->_bf = 0;
+			subL->_bf = -1;
+			subLR->_bf = 0;
+		}
+	}
+
+
+
+	///////
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+			return;
+
+		_InOrder(root->_left);
+		cout << root->_kv.first << ":" << root->_kv.second << endl;
+		_InOrder(root->_right);
+	}
+
+	void InOrder()
+	{
+		_InOrder(_root);
+	}
+
+	int Height(Node* root)
+	{
+		if (root == nullptr)
+			return 0;
+
+		int leftHeight = Height(root->_left);
+		int rightHeight = Height(root->_right);
+
+		return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+	}
+
+	bool _IsBalance(Node* root)
+	{
+		if (root == nullptr)
+			return true;
+
+		int leftHeight = Height(root->_left);
+		int rightHeight = Height(root->_right);
+
+		return abs(leftHeight - rightHeight) < 2
+			&& _IsBalance(root->_left)
+			&& _IsBalance(root->_right);
+	}
+
+	bool IsBalance()
+	{
+		return _IsBalance(_root);
+	}
+private:
+	Node* _root = nullptr;
+};
+
+
+void TestAVLTree()
+{
+
+	int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	//int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+	AVLTree<int, int> t;
+	for (auto e : a)
+	{
+		t.Insert(make_pair(e, e));
+	}
+
+	t.InOrder();
+	cout << t.IsBalance() << endl;
+}
