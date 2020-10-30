0da2b497caf85a20e7c79bfae1cee279072e16e6
Hedenghui <1143671646@qq.com>
2020-10-31 00:02:16 +0800

    oj 最小栈

---
+++ 2020.10.30.1.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+#include<stack>
+class MinStack {
+
+public:
+
+	/** initialize your data structure here. */
+
+	MinStack() { }
+
+
+
+	void push(int x) {
+
+		_st.push(x);
+
+		if (_minst.empty() || _minst.top() >= x)
+
+		{
+
+			_minst.push(x);
+
+		}
+
+	}
+
+
+
+	void pop() {
+
+		if (_st.top() == _minst.top())
+
+			_minst.pop();
+
+
+
+		_st.pop();
+
+	}
+
+
+
+	int top() {
+
+		return _st.top();
+
+	}
+
+
+
+	int getMin() {
+
+		return _minst.top();
+
+	}
+
+
+
+	stack<int> _st;
+
+	stack<int> _minst;
+
+};
+
+
+int main()
+{
+	return 0;
+}
+/**
+
+* Your MinStack object will be instantiated and called as such:
+
+* MinStack* obj = new MinStack();
+
+* obj->push(x);
+
+* obj->pop();
+
+* int param_3 = obj->top();
+
+* int param_4 = obj->getMin();
+
+*/
