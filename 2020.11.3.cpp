c7388d7d774f2a8802db13b39a80f21b56a8bb90
Hedenghui <1143671646@qq.com>
2020-11-03 23:51:43 +0800

    oj 根据二叉树创建字符串

---
+++ 2020.11.3.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+#include<string>
+struct TreeNode {
+    int val;
+	TreeNode *left;
+    TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+	string tree2str(TreeNode* t) {
+		string str;
+		if (t == NULL)
+		{
+			return str;
+		}
+		str += to_string(t->val);
+		if (t->left)
+		{
+			str += '(';
+			str += tree2str(t->left);
+			str += ')';
+		}
+		else if (t->right)
+		{
+			str += "()";
+		}
+		if (t->right)
+		{
+			str += '(';
+			str += tree2str(t->right);
+			str += ')';
+
+		}
+		return str;
+	}
+};
+int main()
+{
+	return 0;
+}
