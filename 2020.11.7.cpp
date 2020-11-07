c5e3296b928d6f07c5967a0cb9f30d3bb36fbd4d
Hedenghui <1143671646@qq.com>
2020-11-07 19:53:39 +0800

    oj 二叉搜索树与双向链表

---
+++ 2020.11.7.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+	val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+	void ConvertList(TreeNode* cur, TreeNode*& prev)
+	{
+		if (cur == NULL)
+			return;
+		ConvertList(cur->left, prev);
+
+		cur->left = prev;
+
+		if (prev)
+			prev->right = cur;
+		prev = cur;
+		ConvertList(cur->right, prev);
+
+	}
+	TreeNode* Convert(TreeNode* pRootOfTree)
+	{
+		TreeNode* prev = NULL;
+		ConvertList(pRootOfTree, prev);
+		TreeNode* head = pRootOfTree;
+		while (head&&head->left)
+			head = head->left;
+		return head;
+	}
+};
+int main()
+{
+	return 0;
+}
