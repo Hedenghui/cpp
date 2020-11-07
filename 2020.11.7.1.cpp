3a3d1298c75485a5832effed21c606f8039129c6
Hedenghui <1143671646@qq.com>
2020-11-07 20:33:06 +0800

    oj 二叉树的最近公共祖先

---
+++ 2020.11.7.1.cpp
@@ -0,0 +1,53 @@
+
+#include<iostream>
+using namespace std;
+
+
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool find(TreeNode* root, TreeNode* x)
+	{
+		if (root == NULL)
+			return false;
+		return root == x || find(root->left, x) || find(root->right, x);
+
+	}
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == NULL)
+			return NULL;
+
+		if (root == p || root == q)
+			return root;
+
+		bool pInLeft, pInRight, qInLeft, qInRight;
+
+		pInLeft = find(root->left, p);
+		pInRight = !pInLeft;
+
+		qInLeft = find(root->left, q);
+		qInRight = !qInLeft;
+
+		if ((pInLeft&&qInRight) || (pInRight&&qInLeft))
+			return root;
+		if (pInLeft&&qInLeft)
+			return lowestCommonAncestor(root->left, p, q);
+		if (pInRight&&qInRight)
+			return lowestCommonAncestor(root->right, p, q);
+
+		return NULL;
+
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
